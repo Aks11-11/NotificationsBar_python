f3570cf7fdf0799d9166cb9658abc24ad007989d
Aks11-11 <aksshatvashisht@gmail.com>
2024-02-29 19:26:03 +0530

    11-11

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+
+notify("Rest", "Break lele bhai")
